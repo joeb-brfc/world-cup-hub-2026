b6343a872418bf28d1d70d56b89ecdaeadbe07e8
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-05-14 14:29:09 +0100

    Create Stadium model and admin registration

--- predictor/admin.py
+++ predictor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import Stadium
 
 # Register your models here.
+admin.site.register(Stadium, StadiumAdmin)
+class StadiumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'capacity')
+    search_fields = ('name', 'city')
--- predictor/models.py
+++ predictor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Stadium(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    capacity = models.PositiveBigIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
