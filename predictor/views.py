7a5cea7c43658cac0f54666c19c3730a19b4a56e
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-05-24 20:37:46 +0100

    Add stage filtering logic to fixture view

--- predictor/views.py
+++ predictor/views.py
@@ -7,9 +7,26 @@ def home(request):
     return render(request, "predictor/home.html")
 
 def fixture_list(request):
-    fixtures = Fixture.objects.all()
+
+    group_fixtures = Fixture.objects.filter(stage="group")
+
+    last_32_fixtures = Fixture.objects.filter(stage="last_32")
+
+    last_16_fixtures = Fixture.objects.filter(stage="last_16")
+
+    quarter_final_fixtures = Fixture.objects.filter(stage="quarter_final")
+
+    semi_final_fixtures = Fixture.objects.filter(stage="semi_final")
+
+    final_fixtures = Fixture.objects.filter(stage="final")
 
     context = {
-        "fixtures": fixtures
+        "group_fixtures": group_fixtures,
+        "last_32_fixtures": last_32_fixtures,
+        "last_16_fixtures": last_16_fixtures,
+        "quarter_final_fixtures": quarter_final_fixtures,
+        "semi_final_fixtures": semi_final_fixtures,
+        "final_fixtures": final_fixtures,
     }
+
     return render(request, "predictor/fixture_list.html", context)
