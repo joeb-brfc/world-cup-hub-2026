946ba33a4d3f6b1744d0c0d7cef0e174441664ab
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-05-24 20:26:35 +0100

    Fix fixture template path naming error

--- predictor/urls.py
+++ predictor/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("fixtures/", views.fixture_list, name="fixtures"),
 ]
--- predictor/views.py
+++ predictor/views.py
@@ -6,10 +6,10 @@ from .models import Fixture
 def home(request):
     return render(request, "predictor/home.html")
 
-def fixtures_list(request):
-    fixtures = Fixturee.objects.all()
+def fixture_list(request):
+    fixtures = Fixture.objects.all()
 
     context = {
         "fixtures": fixtures
     }
-    return render(request, "predictor/fixtures_list.html", context)
+    return render(request, "predictor/fixture_list.html", context)
