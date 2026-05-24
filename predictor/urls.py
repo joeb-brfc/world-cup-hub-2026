5247d0c0eab1ab354c5416016a74ff0a59a7f819
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-05-24 21:14:37 +0100

    Create prediction route

--- predictor/urls.py
+++ predictor/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("fixtures/", views.fixture_list, name="fixtures"),
+    path(
+        "fixtures/<int:fixture_id>/predict/",
+        views.create_prediction,
+        name="create_prediction",
+    ),
 ]
