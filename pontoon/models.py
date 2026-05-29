c4a5811791983006479f6f9211c1acb719809d50
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-05-29 14:00:26 +0100

    Create PontoonBall model

--- pontoon/models.py
+++ pontoon/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from predictor.models import Team
 
-# Create your models here.
+
+class PontoonBall(models.Model):
+    number = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return f"Football {self.number}"
