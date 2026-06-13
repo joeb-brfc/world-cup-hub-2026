bcb25b465d7b1126b18c07d49a177eeb88ce25e2
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-06-13 16:11:13 +0100

    Add Pontoon payment success view

--- pontoon/views.py
+++ pontoon/views.py
@@ -1,7 +1,9 @@
+import stripe
+from django.conf import settings
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import PontoonBall
+from .models import PontoonBall, PontoonAccess
 
 
 @login_required
@@ -70,3 +72,24 @@ def confirm_ball(request, ball_id):
             "ball": ball,
         }
     )
+
+@login_required
+def pontoon_checkout(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    intent = stripe.PaymentIntent.create(
+        amount=settings.STRIPE_PONTOON_PRICE,
+        currency="gbp",
+        metadata={
+            "user_id": request.user.id,
+            "username": request.user.username,
+            "product": "World Cup Pontoon Access",
+        },
+    )
+
+    context = {
+        "stripe_public_key": settings.STRIPE_PUBLIC_KEY,
+        "client_secret": intent.client_secret,
+    }
+
+    return render(request, "pontoon/pontoon_checkout.html", context)
