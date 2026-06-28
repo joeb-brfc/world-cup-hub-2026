271d86baa73cef1b169f4b2b633d9e155f04f3c2
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-06-28 14:22:44 +0100

    Verify Stripe webhook signatures

--- pontoon/webhooks.py
+++ pontoon/webhooks.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.http import HttpResponse
+import stripe
 
 
 def webhook(request):
@@ -6,6 +8,22 @@ def webhook(request):
     Receives webhook events from Stripe.
     """
 
-    print("Webhook received")
+    payload = request.body
+    sig_header = request.META.get("HTTP_STRIPE_SIGNATURE")
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload,
+            sig_header,
+            settings.STRIPE_WH_SECRET,
+        )
+
+    except ValueError:
+        return HttpResponse(status=400)
+
+    except stripe.error.SignatureVerificationError:
+        return HttpResponse(status=400)
+
+    print("Webhook verified")
 
     return HttpResponse(status=200)
